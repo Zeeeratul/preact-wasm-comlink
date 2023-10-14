02cc374fdf6eefe25ee60bc1a9473ff0ee83378e
Zeeeratul <celestdelahaye@gmail.com>
2023-10-14 20:34:12 +0200

    watch assembly folder

--- back-end/assembly/gameboy.ts
+++ back-end/assembly/gameboy.ts
@@ -6,6 +6,10 @@ export default class Gameboy {
   private ppu: PPU = null!;
 
   constructor(romBuffer: Uint8Array) {
+    if (romBuffer.length <= 0) {
+      throw new Error("romBuffer is empty or has an invalid size.");
+    }
+
     this.memory = new Memory(romBuffer);
     this.ppu = new PPU(this.memory);
   }
